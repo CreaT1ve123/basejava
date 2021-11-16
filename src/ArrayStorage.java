70427d1aa543c0f7b605b93f23b36439dca6c59e
CreaT1ve123 <vangelis.kondratevi@gmail.com>
2021-11-16 12:48:48 +0300

    Resume storage size declared as instance variable.

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -5,21 +5,22 @@ import java.util.Arrays;
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    private int size = 0;
 
     void clear() {
-        int size = size();
         for (int i = 0; i < size; i++) {
             storage[i] = null;
         }
+        size = 0;
     }
 
     void save(Resume r) {
-        storage[size()] = r;
+        storage[size++] = r;
     }
 
     Resume get(String uuid) {
         Resume resume = null;
-        for (int i = 0; i < size(); i++) {
+        for (int i = 0; i < size; i++) {
             if (storage[i].toString().equals(uuid)) {
                 resume = storage[i];
             }
@@ -28,13 +29,13 @@ public class ArrayStorage {
     }
 
     void delete(String uuid) {
-        int size = size();
         for (int i = 0; i < size; i++) {
             if (storage[i].equals(get(uuid))) {
                 for (int j = i; j < size - 1; j++) {
                     storage[j] = storage[j + 1];
                 }
                 storage[size - 1] = null;
+                size--;
                 break;
             }
         }
@@ -44,17 +45,10 @@ public class ArrayStorage {
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return Arrays.copyOfRange(storage, 0, size());
+        return Arrays.copyOfRange(storage, 0, size);
     }
 
     int size() {
-        int size = 0;
-        for (int i = 0; i < storage.length; i++) {
-            if (storage[i] == null) {
-                break;
-            }
-            size++;
-        }
         return size;
     }
 }
